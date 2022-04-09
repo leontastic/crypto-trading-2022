d82cbb591543ac9fb14b21577cfaa3207a367db1
Leon Li <leon.li.1025@gmail.com>
2022-04-09 14:30:14 -0400

    implement coinbase websocket subscriber

---
+++ src/constants.ts
@@ -0,0 +1,11 @@
+export const COINBASE_EXCHANGE_WEBSOCKET_URL =
+  'wss://ws-feed.exchange.coinbase.com'
+
+export const EXIT_EVENTS = [
+  'exit',
+  'SIGINT',
+  'SIGUSR1',
+  'SIGUSR2',
+  'uncaughtException',
+  'SIGTERM',
+]
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log('hello world')
+import process from 'node:process'
+import { webSocket, WebSocketSubjectConfig } from 'rxjs/webSocket'
+import ws from 'ws'
+import { COINBASE_EXCHANGE_WEBSOCKET_URL, EXIT_EVENTS } from './constants'
+
+const subscription = {
+  product_ids: ['ETH-USD'],
+  channels: ['ticker'],
+}
+
+const coinbase$ = webSocket({
+  url: COINBASE_EXCHANGE_WEBSOCKET_URL,
+  WebSocketCtor: ws,
+  openObserver: {
+    next() {
+      coinbase$.next({ type: 'subscribe', ...subscription })
+    },
+  },
+  closingObserver: {
+    next() {
+      coinbase$.next({ type: 'unsubscribe', ...subscription })
+    },
+  },
+} as unknown as WebSocketSubjectConfig<Record<string, unknown>>)
+
+coinbase$.subscribe(console.log)
+
+for (const eventType of EXIT_EVENTS)
+  process.on(eventType, () => {
+    coinbase$.complete()
+  })
